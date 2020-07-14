df0f98d7106c77315867ee94fb0a8b3e9343eb90
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-07-14 05:33:33 +0100

    feat: Added email validator

--- src/service/authentication.service.ts
+++ src/service/authentication.service.ts
@@ -14,6 +14,9 @@ import { LoginDto } from '../dto/auth/login.dto';
 import { EventBus } from '@nestjs/cqrs';
 import { AuthenticationUtils } from '../common/utils/authentication-utils.service';
 import { NewAccountSignUpEvent } from '../event/new-account-sign-up.event';
+import { MailerService } from '@nestjs-modules/mailer';
+import { TokenExpiredError } from 'jsonwebtoken';
+import { IllegalArgumentException } from '../exception/IllegalArgumentException';
 
 @Injectable()
 export class AuthenticationService {
@@ -62,6 +65,33 @@ export class AuthenticationService {
       this.eventBus.publish(new NewAccountSignUpEvent(portalAccount));
       return portalUser;
     });
+  }
+
+
+  public verifyUserBearerToken(bearerToken: string): Promise<PortalUser> {
+    return this.authenticationUtils
+      .verifyBearerToken(bearerToken)
+      .then((decoded: { sub: string }) => {
+        return this.connection.getCustomRepository(PortalUserRepository).findOneItem({
+          id: Number(decoded.sub),
+          status: GenericStatusConstant.ACTIVE,
+        });
+      }).then((portalUser: PortalUser) => {
+        if (!portalUser) {
+          throw new UnauthorizedException('User is not active');
+        }
+        delete portalUser.password;
+        return Promise.resolve(portalUser);
+      }).catch((error) => {
+        if (error instanceof TokenExpiredError) {
+          const tokenError = error as TokenExpiredError;
+          throw new IllegalArgumentException(tokenError.message);
+        }
+        if (error instanceof UnauthorizedException) {
+          throw new IllegalArgumentException('Portal user is not authorised to login');
+        }
+        throw  error;
+      });
   }
 
   public async loginUser(loginDto: LoginDto): Promise<string> {
