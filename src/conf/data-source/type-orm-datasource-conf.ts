5798c8f89885bab903d855956d7a7ece61b1c09f
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-08-02 18:07:16 +0100

    quickfix: Removed datasource from git ignore.

---
+++ src/conf/data-source/type-orm-datasource-conf.ts
@@ -0,0 +1,36 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export class TypeOrmDatasourceConf {
+
+  constructor(private configService: ConfigService) {
+  }
+
+  public isProduction() {
+    const mode = this.configService.get('ENV', 'DEV');
+    return mode === 'PROD';
+  }
+
+  public refreshSchema() {
+
+    const mode = this.configService.get('ENV', 'DEV');
+    return mode === 'test';
+  }
+
+  public getTypeOrmConfig(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('DB_HOST', 'localhost'),
+      port: this.configService.get<number>('DB_PORT', 5432),
+      password: this.configService.get<string>('DB_PASSWORD', 'postgres'),
+      username: this.configService.get('DB_USERNAME', 'postgres'),
+      database: this.configService.get('DB_DATABASE', 'postgres'),
+      dropSchema: this.refreshSchema(),
+      logging: this.configService.get('SHOW_LOG', false) === 'true',
+      entities: [__dirname + '/../../domain/entity/*.entity{.js,.ts}'],
+      synchronize: true,
+      ssl: this.isProduction(),
+    };
+  }
+
+}
