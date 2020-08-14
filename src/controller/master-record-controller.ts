f3c09b0eb53bb0ebb6380bcf40b4d18a6b3459d5
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-08-14 22:39:59 +0100

    fix

---
+++ src/controller/master-record-controller.ts
@@ -0,0 +1,7 @@
+import { BaseController } from './BaseController';
+import { Controller } from '@nestjs/common';
+
+@Controller('master-records')
+export class MasterRecordController extends BaseController {
+
+}
