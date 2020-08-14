4808e399f3603ee4d516b26f8d495f353a12b56c
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-08-14 23:11:25 +0100

    feature: Added master records.

--- src/controller/master-record-controller.ts
+++ src/controller/master-record-controller.ts
@@ -1,7 +1,35 @@
 import { BaseController } from './BaseController';
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Connection } from 'typeorm';
+import { BankRepository } from '../dao/bank.repository';
+import { GenericStatusConstant } from '../domain/enums/generic-status-constant';
+import { ApiResponseDto } from '../dto/api-response.dto';
+import { CountryRepository } from '../dao/country.repository';
+import { Public } from '../conf/security/annotations/public';
 
+@Public()
 @Controller('master-records')
 export class MasterRecordController extends BaseController {
 
+  constructor(private readonly connection: Connection) {
+    super();
+  }
+
+  @Get('banks')
+  public async getBanks() {
+    let banks = await this
+      .connection
+      .getCustomRepository(BankRepository)
+      .find({ status: GenericStatusConstant.ACTIVE });
+    return new ApiResponseDto(banks);
+  }
+
+  @Get('counties')
+  public async getCountries() {
+    let countries = await this
+      .connection
+      .getCustomRepository(CountryRepository)
+      .find({ status: GenericStatusConstant.ACTIVE });
+    return new ApiResponseDto(countries);
+  }
 }
