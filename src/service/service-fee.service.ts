9c2ee2c0041804b702187d0219c4bf26b855020e
oluwatobi Adeenkan <tadenekan@dlabs.cloud>
2020-09-14 10:36:53 +0100

    fix: Resolved service fee.

--- src/service/service-fee.service.ts
+++ src/service/service-fee.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { ServiceFeeRequestDto } from '../dto/service-fee-request.dto';
 import { Association } from '../domain/entity/association.entity';
-import { Connection } from 'typeorm';
+import { Connection, EntityManager } from 'typeorm';
 import { ServiceFee } from '../domain/entity/service.fee.entity';
 import { ServiceFeeCodeSequence } from '../core/sequenceGenerators/service-fee-code.sequence';
 import * as moment from 'moment';
+import { PortalUser } from '../domain/entity/portal-user.entity';
+// import { SubscriptionService } from './subscription.service';
+import { EventBus } from '@nestjs/cqrs';
 
 @Injectable()
 export class ServiceFeeService {
   constructor(private readonly connection: Connection,
+              private readonly eventBus: EventBus,
+              // private readonly subscriptionService: SubscriptionService,
               private readonly serviceFeeCodeSequence: ServiceFeeCodeSequence) {
   }
 
-  public async createService(serviceFeeRequestDto: ServiceFeeRequestDto, association: Association) {
+  public async createService(serviceFeeRequestDto: ServiceFeeRequestDto,
+                             association: Association,
+                             recipients?: PortalUser[]) {
     return this.connection.transaction(async entityManager => {
       let serviceFee = new ServiceFee();
       serviceFee.association = association;
@@ -22,11 +29,15 @@ export class ServiceFeeService {
       serviceFee.name = serviceFeeRequestDto.name;
       serviceFee.type = serviceFeeRequestDto.type;
       serviceFee.description = serviceFeeRequestDto.description;
-      let todaysDate = moment(serviceFeeRequestDto.firstBillingDate, 'DD/MM/YYYY').startOf('day').toDate();
-      serviceFee.firstBillingDate = todaysDate;
-      serviceFee.nextBillingDate = todaysDate;
-      return entityManager.save(serviceFee);
+      serviceFee.firstBillingDate = moment(serviceFeeRequestDto.firstBillingDate, 'DD/MM/YYYY')
+        .startOf('day')
+        .toDate();
+      serviceFee = await entityManager.save(serviceFee);
+     // this.eventBus.publish(new ServiceFeeCreationEvent(serviceFee, recipients));
+      return serviceFee;
     });
 
   }
+
+
 }
