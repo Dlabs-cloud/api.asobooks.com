f16f996f8f4f707de79e08a56a18a02fd1a6ab1a
Oluwatobi Adenekan <tobi>
2021-01-14 08:55:03 +0100

    Added activity logs

--- src/event/activity-log.event.ts
+++ src/event/activity-log.event.ts
@@ -3,27 +3,12 @@ import { ActivityTypeConstant } from '../domain/enums/activity-type-constant';
 import { Association } from '../domain/entity/association.entity';
 
 export class ActivityLogEvent {
-  private description: string;
-  private association: Association;
-
-  constructor(private readonly portalUser: PortalUser,
-              public readonly activityType: ActivityTypeConstant) {
-  }
-
-  addDescription(desc: string) {
-    return this;
-  }
-
-  addAssociation(association: Association) {
-    return this;
-  }
 
 
-  build() {
-    const activityLogEvent = new ActivityLogEvent(this.portalUser, this.activityType);
-    activityLogEvent.description = this.description;
-    activityLogEvent.association = this.association;
-    return activityLogEvent;
+  constructor(public readonly portalUser: PortalUser,
+              public readonly association: Association,
+              public readonly description: string,
+              public readonly type: ActivityTypeConstant) {
   }
 
 
