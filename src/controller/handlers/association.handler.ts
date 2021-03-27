f14bf7517ea264cdc9e396912a606a87e9eff218
Oluwatobi Adenekan <tobi>
2021-03-27 15:34:49 +0100

    Update status of logo.

--- src/controller/handlers/association.handler.ts
+++ src/controller/handlers/association.handler.ts
@@ -8,6 +8,8 @@ import { BankRepository } from '../../dao/bank.repository';
 import { AddressRepository } from '../../dao/address.repository';
 import { BankInfo } from '../../domain/entity/bank-info.entity';
 import { AccountDetailRepository } from '../../dao/account-detail.repository';
+import { GenericStatusConstant } from '../../domain/enums/generic-status-constant';
+import { AssociationFileTypeConstant } from '../../domain/enums/association-file-type.constant';
 
 @Injectable()
 export class AssociationHandler {
@@ -20,7 +22,7 @@ export class AssociationHandler {
     response.name = association.name;
     response.type = association.type;
     return this.connection.getCustomRepository(AssociationFileRepository)
-      .findOne({ association })
+      .findOne({ association, status: GenericStatusConstant.ACTIVE, type: AssociationFileTypeConstant.LOGO })
       .then(fileType => {
         response.logo = fileType?.file?.servingUrl;
       }).then(_ => {
